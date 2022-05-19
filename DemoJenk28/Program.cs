8bf152354b83e5374c19ac55671e24bfa780d536
mallikapps <mallik.techno@gmail.com>
2022-05-19 15:25:43 +0530

    Add project files.

---
+++ DemoJenk28/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoJenk28
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
